7fd29751e16a87580b16ba2d0cc97c1a705a9df1
Mohammad Shahebaz <shahebaz13@gmail.com>
2020-03-21 17:45:12 +0530

    Flaske8 Resolves

--- apis/utils.py
+++ apis/utils.py
@@ -5,5 +5,4 @@ api = Namespace('utils', description='Namespace for utils')
 @api.route('/isalive')
 class IsAlive(Resource):
     def get(self):
-        app.logger.info("Pinging API alive status")
         return {'success': 'I am up and running :)'}
