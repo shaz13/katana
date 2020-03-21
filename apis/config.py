aa1ae7cc90abd9c0716cc58101316c47bddd6f5d
Aditya Soni <soniaditya922@gmail.com>
2020-03-21 16:57:32 +0530

    typos

--- apis/config.py
+++ apis/config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-APP_ROOT    = Path("./core")
-MODELS_ROOT = APP_ROOT / "models" / "logreg.pkl"
-TEMP_CSV    = APP_ROOT / "diabetes.csv"
+APP_ROOT = Path("./core")
+MODEL_ROOT = APP_ROOT / "models" / "logreg.pkl"
+TEMP_CSV = APP_ROOT / "diabetes.csv"
