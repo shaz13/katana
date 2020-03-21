386791e58e762869df201ac7a76bd85af26ed90b
Mohammad Shahebaz <shahebaz13@gmail.com>
2020-03-21 22:01:00 +0530

    Inference

--- apis/inference.py
+++ apis/inference.py
@@ -1,8 +1,38 @@
+import pickle
+import pandas as pd
 from flask_restplus import Namespace, Resource, fields
+from flask import request
+from apis.config import MODEL_ROOT
 
 api = Namespace('inference', description='Namespace for inference')
 
+personModel = api.model('PersonDiabetes', {
+    'Pregnancies': fields.String(required=True),
+    'Glucose': fields.String(required=True),
+    'BloodPressure': fields.String(required=True),
+    'SkinThickness': fields.String(required=True),
+    'Insulin': fields.String(required=True),
+    'BMI': fields.String(required=True),
+    'DiabetesPedigreeFunction': fields.String(required=True),
+    'Age': fields.String(required=True),
+})
+
+
 @api.route('/predict')
+@api.doc(personModel)
+@api.expect(personModel)
 class Inference(Resource):
-    def get(self):
-        return {'success': 'Prediction successful'}
+    def post(self):
+        data = request.json
+        prediction = self.predict(data)
+        return {'success': 'Prediction successful',
+                'probability': str(prediction)
+                }, 200
+
+    def predict(self, data):
+        # load the model from disk
+        model = pickle.load(open(MODEL_ROOT, 'rb'))
+        print(data)
+        instance = pd.DataFrame(data, index=[0])
+        probability = round(model.predict_proba(instance)[:, 1][0], 4)
+        return probability
