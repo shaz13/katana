d8ece858e58840b2c9858f6180dd193721c6abe0
Aditya Soni <soniaditya922@gmail.com>
2020-03-22 22:14:09 +0530

    added colored logging

---
+++ colored_logging.py
@@ -0,0 +1,26 @@
+import logging
+
+class ColorFormatter(logging.Formatter):
+    """Logging Formatter to add colors for warning, errors etc"""
+
+    blue = '\x1b[94m'
+    green = '\x1b[92m'
+    grey = "\x1b[38;21m"
+    yellow = "\x1b[33;21m"
+    red = "\x1b[31;21m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: blue + format + reset,
+        logging.INFO: green + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: bold_red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import yaml
+import logging
 import os
+import logging.config
+import yaml
 
 from flask import Flask
 from apis import api
+from colored_logging import ColorFormatter
 
 with open(r'config.yml') as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
@@ -10,9 +14,12 @@ with open(r'config.yml') as file:
 app = Flask(__name__)
 api.init_app(app)
 
+# create logger
+logging.ColorFormatter = ColorFormatter
+logging.config.fileConfig('gunicorn_logging.conf')
+
 if __name__ == '__main__':
 
     if not os.path.exists("./core/models"):
         os.makedirs("./core/models", exist_ok=True)
-    app.logger.info("Starting the ML Engines")
     app.run(debug=config['DEBUG'], host=config['HOST'], port=config['PORT'])
