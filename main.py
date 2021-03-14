ca02efb08583e8a7e2cd3c3775ea05835572a505
Mohammad Shahebaz <shahebaz13@gmail.com>
2021-03-14 08:44:31 +0530

    Setting Procfile

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ app = FastAPI(title="Katana Fast API Serving", version=1.0)
 app.include_router(iris_ns)
 
 
-# @app.get("/", tags=["redirect"])
-# async def redirect():
-#     return RedirectResponse("/docs")
+@app.get("/", tags=["redirect"])
+async def redirect():
+    return RedirectResponse("/docs")
